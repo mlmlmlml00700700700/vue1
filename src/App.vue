545913e612c0895bc39f9f7a97a780aa2948a393
mlmlmlml00700700700 <128768493+mlmlmlml00700700700@users.noreply.github.com>
2024-03-12 15:29:51 +0900

    리본 추가

--- src/App.vue
+++ src/App.vue
@@ -61,7 +61,7 @@ import { ref, watch, onMounted, computed } from 'vue';
   <main class="app">
     <section class="greeting">
       <h1 class="title"> 
-        What's up
+        What's up 💖 
         <input 
           type="text" 
           placeholder="name here"
